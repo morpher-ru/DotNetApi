b061a96749c0b4dcc77ec0c449f2caadbe2b5aab
Sergey Filenko <sergey@filenko.me>
2017-12-21 18:46:02 +0200

    repo + new .gitignore

---
+++ Morpher.WebService.V3/Client.cs
@@ -0,0 +1,29 @@
+﻿namespace Morpher.WebService.V3
+{
+    public class Client
+        : Russian.IDeclension
+        , Ukrainian.IDeclension
+        , Russian.INumberSpelling
+        , Ukrainian.INumberSpelling
+    {
+        public Russian.IParse Parse(string s, Russian.ParseArgs args = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Ukrainian.IParse Parse(string s, Ukrainian.ParseArgs args = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Spell(decimal n, ref string unit, ICase<Russian.IParadigm> @case)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Spell(decimal n, ref string unit, ICase<Ukrainian.IParadigm> @case)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
