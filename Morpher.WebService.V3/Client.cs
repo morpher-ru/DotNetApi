cd190035d4d7f0cd645139be65ab8ce5694e03fb
Sergey Filenko <sergey@filenko.me>
2017-12-21 18:59:06 +0200

    namespace hell

--- Morpher.WebService.V3/Client.cs
+++ Morpher.WebService.V3/Client.cs
@@ -1,29 +1,48 @@
-﻿namespace Morpher.WebService.V3
+﻿using System;
+
+namespace Morpher.WebService.V3
 {
     public class Client
-        : Russian.IDeclension
-        , Ukrainian.IDeclension
-        , Russian.INumberSpelling
-        , Ukrainian.INumberSpelling
+        : Morpher.Russian.IDeclension
+        , Morpher.Ukrainian.IDeclension
+        , Morpher.Russian.INumberSpelling
+        , Morpher.Ukrainian.INumberSpelling
     {
-        public Russian.IParse Parse(string s, Russian.ParseArgs args = null)
+        private readonly MorpherClient _morpherClient;
+
+        public Client()
         {
-            throw new System.NotImplementedException();
+            _morpherClient = new MorpherClient();
         }
 
-        public Ukrainian.IParse Parse(string s, Ukrainian.ParseArgs args = null)
+        Morpher.Russian.IParse Morpher.Russian.IDeclension.Parse(
+            string s,
+            Morpher.Russian.ParseArgs args)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        public string Spell(decimal n, ref string unit, ICase<Russian.IParadigm> @case)
+        Morpher.Ukrainian.IParse Morpher.Ukrainian.IDeclension.Parse(
+            string s,
+            Morpher.Ukrainian.ParseArgs args)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        public string Spell(decimal n, ref string unit, ICase<Ukrainian.IParadigm> @case)
+        string Morpher.Russian.INumberSpelling.Spell(
+            decimal n,
+            ref string unit,
+            ICase<Morpher.Russian.IParadigm> @case)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        string Morpher.Ukrainian.INumberSpelling.Spell(
+            decimal n,
+            ref string unit,
+            ICase<Morpher.Ukrainian.IParadigm> @case)
+        {
+            throw new NotImplementedException();
         }
     }
 }
